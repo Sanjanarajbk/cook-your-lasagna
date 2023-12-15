2b59a64a111b535891b468860a4a00bcc6552274
Sanjanarajbk <gowdasanjana60.sg@gmail.com>
2023-12-15 21:11:21 +0530

    2nd exercise done

---
+++ src/main/java/Lasagna.java
@@ -0,0 +1,23 @@
+public class Lasagna {
+    // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven(){
+        return 40;
+    }
+
+    // TODO: define the 'remainingMinutesInOven()' method
+
+    public int remainingMinutesInOven(int actual){
+        return 40-actual;
+    }
+
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layers){
+        return layers*2;
+    }
+
+    // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layers,int prep){
+        return layers*2+prep;
+    }
+}
